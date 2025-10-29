69d5b84ae090abfa9ba24a02a444d2e173fd47f5
Anand Venugopalapanicker <anandvpranavam@gmail.com>
2025-10-28 23:14:55 -0400

    added code to plot - but, not working without header; added code to put header for the data; successfully printed the data

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Define column names based on your description
+column_names = ['raw_time', 'voltage', 'ampere', 'soc', 'unknown']
 # load data For a CSV file (like our example)
-df = pd.read_csv('serial1.3-2025-10-22_09-35-11 - Second Charge.log')
-
+df = pd.read_csv('serial1.3-2025-10-22_09-35-11 - Second Charge.log', header=None, names=column_names)
 #df = pd.read_csv('data.txt', sep='\t') #For .txt files
 print(df.head())
+
+
+# # 1. Create a figure and an axes (the "canvas" for our plot)
+# plt.figure(figsize=(10, 6))  # Sets the size of the plot (width, height in inches)
+# # 2. Plot the data
+# # plt.plot(x-axis, y-axis)
+# plt.plot(df['raw_time'], df['voltage'], marker='o', linestyle='-')
+# # 3. Add labels and a title
+# plt.title('Voltage Vs Time')
+# plt.xlabel('Time')
+# plt.ylabel('Voltage (V)')
+# # 4. Add a grid for readability
+# plt.grid(True)
+# # 5. Display the plot
+# plt.show()
